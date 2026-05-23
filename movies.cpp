95685da6af2802784eb6c48c502881e7b2b1ebde
misali <misali@ucsb.com>
2026-05-22 20:39:11 -0700

    sooooo close

--- movies.cpp
+++ movies.cpp
@@ -10,44 +10,46 @@ void print_alphabetical(map<string, double> mbin)
 
 void print_prefixes(vector<string> prefixes, map<string, double> mbin)
 {
-    for(int i = 0; i < prefixes.size(); i++)
+    for (int i = 0; i < prefixes.size(); i++)
     {
-        string best = print_prefix(prefixes[i], mbin);
+        string highest = print_prefix(prefixes[i], mbin);
 
-        if(best.empty())
-            cout << "No movies found with prefix " << prefixes[i] << endl;
+        if (highest.empty())
+            cout << "No movies found with prefix "<< prefixes[i] << endl;
         else
-            cout << "\nBest movie with prefix " << prefixes[i] << " is: " << best << endl;
-        if(i != prefixes.size() - 1)
-            cout << endl;
+            cout << "highest movie with prefix " << prefixes[i] << " is: " << highest << endl;
+
+        if (i != prefixes.size() - 1)
+            cout << endl; 
     }
 }
 
 string print_prefix(string prefix, map<string, double> mbin)
 {
-    vector<pair<double,string>> prefixed;
+    vector<pair<double, string>> prefixed;
+    int plength = prefix.length();
 
-    for(auto movie : mbin)
+    for (auto movie : mbin)
     {
-        if(movie.first.compare(0, prefix.length(), prefix) == 0)
+        if (movie.first.compare(0, plength, prefix) == 0)
             prefixed.push_back({movie.second, movie.first});
     }
-
-    if(prefixed.empty())
+    if (prefixed.empty())
         return "";
 
     sort(prefixed.begin(), prefixed.end(),
         [](const pair<double,string>& a,
            const pair<double,string>& b)
         {
-            if(a.first == b.first)
-                return a.second < b.second;
-            return a.first > b.first;
+            if (a.first == b.first)
+                return a.second < b.second;  
+            return a.first > b.first; 
         });
 
-    for(auto &m : prefixed)
+    for (auto &m : prefixed)
         cout << m.second << ", " << m.first << endl;
 
+    cout << endl;
     return prefixed[0].second + " with rating " + castStr(prefixed[0].first);
 }
 
