4df6841b2082c8085bbd18bae20585b326e73e18
misali <misali@ucsb.com>
2026-05-22 20:28:57 -0700

    another overahaul

--- movies.cpp
+++ movies.cpp
@@ -12,60 +12,43 @@ void print_prefixes(vector<string> prefixes, map<string, double> mbin)
 {
     for(int i = 0; i < prefixes.size(); i++)
     {
-        string highest = print_prefix(prefixes[i], mbin);
+        string best = print_prefix(prefixes[i], mbin);
 
-        if(highest.empty())
+        if(best.empty())
             cout << "No movies found with prefix " << prefixes[i] << endl;
         else
-            cout << "Best movie with prefix " << prefixes[i] << " is: " << highest << endl;
+            cout << "\nBest movie with prefix " << prefixes[i] << " is: " << best << endl;
+        if(i != prefixes.size() - 1)
+            cout << endl;
     }
 }
 
 string print_prefix(string prefix, map<string, double> mbin)
 {
-    struct Compare {
-        bool operator()(const pair<double,string>& a, const pair<double,string>& b)
-        {
-            if(a.first == b.first)
-                return a.second > b.second;
-
-            return a.first < b.first;
-        }
-    };
-
-    priority_queue<
-        pair<double,string>,
-        vector<pair<double,string>>,
-        Compare
-    > premovie;
-
-    string greatest;
-    int plength = prefix.length();
+    vector<pair<double,string>> prefixed;
 
     for(auto movie : mbin)
     {
-        if(movie.first.compare(0, plength, prefix) == 0)
-            premovie.push({movie.second, movie.first});
+        if(movie.first.compare(0, prefix.length(), prefix) == 0)
+            prefixed.push_back({movie.second, movie.first});
     }
 
-    if(!premovie.empty())
-    {
-        greatest = premovie.top().second
-                 + " with rating "
-                 + castStr(premovie.top().first);
-    }
+    if(prefixed.empty())
+        return "";
 
-    while(!premovie.empty())
-    {
-        cout << premovie.top().second
-             << ", "
-             << castStr(premovie.top().first)
-             << endl;
+    sort(prefixed.begin(), prefixed.end(),
+        [](const pair<double,string>& a,
+           const pair<double,string>& b)
+        {
+            if(a.first == b.first)
+                return a.second < b.second;
+            return a.first > b.first;
+        });
 
-        premovie.pop();
-    }
+    for(auto &m : prefixed)
+        cout << m.second << ", " << m.first << endl;
 
-    return greatest;
+    return prefixed[0].second + " with rating " + castStr(prefixed[0].first);
 }
 
 string castStr(double val)
