92988f069484d93653195b847fcc90a7f8aecb31
misali <misali@ucsb.com>
2026-05-22 20:09:40 -0700

    wef

--- movies.cpp
+++ movies.cpp
@@ -1,6 +1,5 @@
 #include "movies.h"
 
-
 void print_alphabetical(map<string, double> mbin)
 {
     for(auto movie : mbin) //since std::map is automatically sorted in ascending order this should be alphabetical order
@@ -9,47 +8,78 @@ void print_alphabetical(map<string, double> mbin)
     }
 }
 
-
 void print_prefixes(vector<string> prefixes, map<string, double> mbin)
 {
     vector<string> largest_prefix;
     int size = prefixes.size();
+
     for(int i = 0; i < size; i++)
     {
         largest_prefix.push_back(print_prefix(prefixes[i], mbin));
+
         if(!largest_prefix[i].empty())
-        cout << endl;
+            cout << endl;
     }
+
     for(int i = 0; i < size; i++)
     {
         if(largest_prefix[i].empty())
             cout << "No movies found with prefix " << prefixes[i] << endl;
         else
-            cout << "Best movie with prefix " << prefixes[i] << " is: " << largest_prefix[i] << endl;
+            cout << "Best movie with prefix " << prefixes[i]
+                 << " is: " << largest_prefix[i] << endl;
     }
 }
 
 string print_prefix(string prefix, map<string, double> mbin)
 {
+    struct Compare {
+        bool operator()(const pair<double,string>& a, const pair<double,string>& b)
+        {
+            if(a.first == b.first)
+                return a.second > b.second;
+
+            return a.first < b.first;
+        }
+    };
+
+    priority_queue<
+        pair<double,string>,
+        vector<pair<double,string>>,
+        Compare
+    > premovie;
+
     string greatest;
-    priority_queue<pair<double, string>> premovie;
     int plength = prefix.length();
+
     for(auto movie : mbin)
     {
-        if((movie.first).substr(0,plength) == prefix)
+        if(movie.first.compare(0, plength, prefix) == 0)
             premovie.push({movie.second, movie.first});
     }
+
     if(!premovie.empty())
-        greatest = premovie.top().second + " with rating " + castStr(premovie.top().first);
+    {
+        greatest = premovie.top().second
+                 + " with rating "
+                 + castStr(premovie.top().first);
+    }
+
     while(!premovie.empty())
     {
-        cout << premovie.top().second << ", " << castStr(premovie.top().first) << endl;
+        cout << premovie.top().second
+             << ", "
+             << castStr(premovie.top().first)
+             << endl;
+
         premovie.pop();
     }
+
     return greatest;
 }
 
-string castStr(double val) {
+string castStr(double val)
+{
     ostringstream oss;
     oss << val;
     return oss.str();
